8015fae0f75922f4a0fe86b7f62e7977aeb780a0
Kelly Campos <kelly01201601@gmail.com>
2025-07-02 21:42:22 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -10,8 +10,8 @@ abstract class Conta {
 		this.titular= titular;
 		this.saldo=0.0;
 	}
-	// encapsulamnto: metodos para acessar atributos privados QUEM ACESSA É A CONTA
-	// quando for chamdo vai retornar o titular e saldo, GET é usado para acessas algum dado
+	// Métodos para acessar atributos privados
+	// Quando for chamdo vai retornar o titular e saldo
 	public String getTitular() {
 		return titular;
 	}
@@ -19,35 +19,35 @@ abstract class Conta {
 	public double getSaldo() {
 		return saldo;
 	}
-	//metodo protegido acessado smente pea subclasse
-	//construtor que vai fazer a alteraçãp do valor da conta
+	
+	//Construtor que vai fazer a alteraçãp do valor da conta
 	protected void alterarSaldo(double valor) {
 		this.saldo += valor;
 		
 		
 	}
-	//metodos abstratos: exigem iplementação nas subclasses
-	// modelos que dizem como depositar ou sacar
+	
+	// Modelos que dizem como depositar ou sacar
 	public abstract void depositar(double valor);
 	public abstract void sacar (double valor);
 	
 }	
-//classe cncreta que implementa os metodos abstratos
-// classe de conta corrente que extende da classe conta, ou seja, possue todos os dados que a classe conta tem
+
+// Classe de conta corrente que extende da classe conta
 class ContaCorrente extends Conta{
-	//conta vai receber o nome do titular
+	//Conta vai receber o nome do titular
 	public ContaCorrente(String titular) {
 		super(titular);
 }
-	//aqui, se o valor que o titular quiser depositar for maior que 0, será depositado
+	//Será depositado omente se o valor for maior que 0
 	public void depositar (double valor) {
 		if (valor>0) {
 			alterarSaldo(valor);
 			System.out.println("deposito de R& "+ valor + " realizado.");
 		}
 	}
-	//para sacar o valor que está sendo solicitado precisa já está na conta, sendo assim, 
-	//condição para verificar se tem o valor na conta
+	
+	//Condição para verificar se tem o valor na conta
 	public void sacar (double valor) {
 		if(valor>0 && getSaldo()>=valor) {
 			alterarSaldo(-valor);
